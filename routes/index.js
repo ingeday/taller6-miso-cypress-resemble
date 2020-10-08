6b771599c48e853b587cfdde1076bc58a02d2d0e
Eday Gonzalez <edaygonzalez@ingenieros.com>
2020-10-07 22:51:24 -0500

    Se agregan templates con pug y prueba de llamado de imagen

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
+const output_path=path.join(__dirname,'../testingVRT');
+const data=[];
+fs.readdir(output_path, function(err, files) {
+    if(err) {
+        return console.log(`error leyendo ${output_path} => ${err}`)
+    }
+    let filesArray=[];
+    
+    //files.forEach(function(file){
+      //  filesArray.push(file);
+    //})
+    
+    
+    for(i=1; i<5; i++) {
+        let tempData=[]; anyData=false;
+        iter=0;
+        files.forEach(function(el){
+        
+            if(el.startsWith(i)) {
+                anyData=true;
+                tempData.push(el);
+            }
+            
+        })
+        if(anyData)
+            data.push(tempData);
+    }
+    console.log(data)
+})
+
 router.get('/', (req, res) => {
-  res.send('It works!');
+  res.render('form',{ title: 'Registration form', data:data });
 });
 
 module.exports = router;
